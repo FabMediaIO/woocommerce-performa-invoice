e4b769b23a8dec2f2dfd5c9525d49e22b7464b55
Faisal Awan <faisalawan.live@gmail.com>
2018-08-30 01:59:10 +0500

    Revert "Revert "add variable product in invoice""
    This reverts commit 11e0c9f40f3b8c2afe2b568c66116fc32f773daf.

--- admin/partials/woocommerce-performa-invoice-excel-generate.php
+++ admin/partials/woocommerce-performa-invoice-excel-generate.php
@@ -41,66 +41,93 @@ $file_title = $wox_option_data['wox_file_title'];
 $header_content = str_replace(array('<br/>', '<br />', '</br>'), '<w:br />', $header_content);
 $thankyou_message = $wox_option_data['wox_thankyou_message'];
 
-$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor($woe_template_file);
-
-$templateProcessor->setValue(
-    array(
-        'title',
-        'file_title',
-        //'header_content',
-        'description',
-        'date',
-        'order',
-        'customeraddress',
-        'customerphone',
-        'customeremail',
-        'customername',
-        'subtotalamount',
-        'paymentmethod',
-        'totalamount',
-        'additional_notes',
-        'thankyou_message',
-    ), 
-    array(
-        get_option( 'blogname' ),
-        $file_title,
-        //$header_content,
-        get_option( 'blogdescription' ),
-        wc_format_datetime( $order->get_date_created() ),
-        $order->get_order_number(),
-        $get_formatted_billing_address,
-        $order->get_billing_phone(),
-        $order->get_billing_email(),
-        $order->get_billing_first_name()." ".$order->get_billing_last_name(),
-        $cart_subtotal,
-        $payment_method,
-        $order_total,
-        $additional_notes,
-        $thankyou_message
-    )
+$keys = array(
+    'title',
+    'file_title',
+    //'header_content',
+    'description',
+    'date',
+    'order',
+    'customeraddress',
+    'customerphone',
+    'customeremail',
+    'customername',
+    'subtotalamount',
+    'paymentmethod',
+    'totalamount',
+    'additional_notes',
+    'thankyou_message',
+    'total_items'
 );
 
+$values = array(
+    get_option( 'blogname' ),
+    $file_title,
+    //$header_content,
+    get_option( 'blogdescription' ),
+    wc_format_datetime( $order->get_date_created() ),
+    $order->get_order_number(),
+    $get_formatted_billing_address,
+    $order->get_billing_phone(),
+    $order->get_billing_email(),
+    $order->get_billing_first_name()." ".$order->get_billing_last_name(),
+    $cart_subtotal,
+    $payment_method,
+    $order_total,
+    $additional_notes,
+    $thankyou_message,
+    $order->get_item_count()
+);
+
+$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor($woe_template_file);
+
+$templateProcessor->setValue($keys, $values);
+
 $templateProcessor->cloneRow('productname', count($order_items));
+
 $order_i = 1;
 foreach ( $order_items as $item_id => $item ) {
+    
     $product = $item->get_product();
     $product_id = $item->get_product_id();
     $product_title = $product->get_title();
+    $get_sku = $product->get_sku();
     
+
     $get_quantity = $item->get_quantity();
     $single_price = wc_price( $order->get_item_total( $item, false, true ), array( 'currency' => $order->get_currency() ) );
     $single_price = strip_tags(str_replace($symbol, '', $single_price));
     $subtotal = $order->get_formatted_line_subtotal( $item );
     $subtotal = strip_tags(str_replace($symbol, '', $subtotal));
 
+    $variable = '';
+
+    // Only for product variation
+    if($product->is_type('variation')){
+
+         // Get the variation attributes
+        $variation_attributes = $product->get_variation_attributes();
+        // Loop through each selected attributes
+        foreach($variation_attributes as $attribute_taxonomy => $term_slug){
+            $attribute_taxonomy = str_replace('attribute_', '', $attribute_taxonomy);
+            $attribute_taxonomy = ucwords($attribute_taxonomy);
+            $variable .= $attribute_taxonomy.' - '.$term_slug.'<w:br />';
+        }
+    }
+    
     $templateProcessor->setValue("productname#$order_i", strip_tags($product_title));
+    $templateProcessor->setValue("productsku#$order_i", $get_sku);
+    $templateProcessor->setValue("product_var#$order_i", $variable);
     $templateProcessor->setValue("productunit#$order_i", strip_tags($single_price));
     $templateProcessor->setValue("productcount#$order_i", $get_quantity);
     $templateProcessor->setValue("productamount#$order_i", strip_tags($subtotal));
+
+    //$templateProcessor->setValue("product_var#$order_i", $variable);
     
 
     $order_i++;
 }
+
 
 if(isset($test)){
     $order_id = wp_generate_password(13, false, false);
